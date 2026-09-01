30d60d7990059c61913908cc4e266847838245ff
Gauransh Arora <gauransharora24317@gmail.com>
2026-09-01 15:54:36 +0530

    sponsor horizontal scroll

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -3,12 +3,20 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import peacock from "../assets/peacock.png";
-import lotus from "../assets/lotus.png";
-import designSymbol from "../assets/design_symbol.png";
+import bird from "../assets/bird.png";
+import sponsorFrameImg from "../assets/sponsor_frame.png";
 
 import "./Sponsors.css";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const SponsorFrame = ({ className = "" }) => (
+  <img 
+    src={sponsorFrameImg} 
+    alt="Sponsor Frame" 
+    className={`sponsor-frame ${className}`} 
+  />
+);
 
 export default function Sponsors() {
   const sectionRef = useRef<HTMLElement>(null);
@@ -59,47 +67,57 @@ export default function Sponsors() {
         
         {/* PEACOCK */}
         <div className="sponsors-peacock">
-          <img src={peacock} alt="Peacock" onLoad={() => ScrollTrigger.refresh()} />
+          <img src={bird} alt="Peacock" onLoad={() => setTimeout(() => ScrollTrigger.refresh(), 100)} />
         </div>
 
         {/* CONTENT */}
         <div className="sponsors-content">
-          <div className="sponsors-title-container">
-            <h2 className="sponsors-title-bg">Sponsors</h2>
-            <h2 className="sponsors-title-fg">SPONSORS</h2>
+          
+          {/* TITLE SPONSOR GROUP */}
+          <div className="sponsor-group title-sponsor-group">
+            <div className="sponsor-intro-texts">
+              <h3 className="sponsor-heading-gothic">Our Sponsors</h3>
+              <h2 className="sponsor-heading-cursive">The Brands That Made Saturnalia Possible</h2>
+            </div>
+            <div className="title-sponsor-layout">
+              <div className="title-sponsor-vertical-text">
+                Title Sponsor
+              </div>
+              <div className="frame-wrapper title-frame">
+                <SponsorFrame />
+              </div>
+            </div>
           </div>
 
-          <div className="sponsors-marquee-area">
-            {/* TOP ROW */}
-            <div className="sponsors-row top-row">
-              {[...Array(6)].map((_, i) => (
-                <div key={`top-${i}`} className="sponsor-symbol-wrapper">
-                  <img src={designSymbol} alt="Design Symbol" className="design-symbol" />
-                  <div className="sponsor-circle"></div>
-                </div>
-              ))}
-            </div>
-
-            {/* SUBTITLE */}
-            <h3 className="sponsors-subtitle">
-              THE WONDERFUL BRANDS THAT MADE SATURNALIA'25 POSSIBLE
-            </h3>
-
-            {/* BOTTOM ROW */}
-            <div className="sponsors-row bottom-row">
-              {[...Array(7)].map((_, i) => (
-                <div key={`bottom-${i}`} className="sponsor-symbol-wrapper">
-                  <img src={designSymbol} alt="Design Symbol" className="design-symbol" />
-                  <div className="sponsor-circle"></div>
+          {/* CO-TITLE SPONSORS GROUP */}
+          <div className="sponsor-group co-title-group">
+            <h3 className="sponsor-heading-gothic">Co-Title Sponsors</h3>
+            <div className="co-title-grid">
+              {[...Array(4)].map((_, i) => (
+                <div key={`co-${i}`} className="frame-wrapper co-title-frame">
+                  <SponsorFrame />
                 </div>
               ))}
             </div>
           </div>
+
+          {/* POWER SPONSORS GROUP */}
+          <div className="sponsor-group power-group">
+            <h3 className="sponsor-heading-gothic">Power Sponsors</h3>
+            <div className="power-grid">
+              {[...Array(21)].map((_, i) => (
+                <div key={`power-${i}`} className="frame-wrapper power-frame">
+                  <SponsorFrame />
+                </div>
+              ))}
+            </div>
+          </div>
+
         </div>
 
         {/* LOTUS */}
         <div className="sponsors-lotus">
-          <img src={lotus} alt="Lotus" onLoad={() => ScrollTrigger.refresh()} />
+          <img src={peacock} alt="Lotus" onLoad={() => setTimeout(() => ScrollTrigger.refresh(), 100)} />
         </div>
 
       </div>
